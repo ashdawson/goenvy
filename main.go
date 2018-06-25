6093f954818e28b17cd760a5cab742a09af4c0b3
ashdawson <ash@ashdawson.com>
2018-06-25 19:15:37 +1000

    Initial Commit

---
+++ main.go
@@ -0,0 +1,46 @@
+package goenvy
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strings"
+)
+
+func Load() {
+	file, err := os.Open(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	setEnvironVars(file)
+	defer file.Close()
+}
+
+func getSystemEnviron() map[string]string {
+	sysEnvMap := make(map[string]string)
+	sysEnv := os.Environ()
+	for _, envVar := range sysEnv {
+		env := strings.Split(envVar, "=")
+		sysEnvMap[env[0]] = env[1]
+	}
+
+	return sysEnvMap
+}
+
+func setEnvironVars(file *os.File) {
+	currEnvMap := make(map[string]string)
+	sysEnvMap := getSystemEnviron()
+	input := bufio.NewScanner(file)
+	for input.Scan() {
+		if strings.Contains(input.Text(), "=") {
+			env := strings.Split(input.Text(), "=")
+			currEnvMap[env[0]] = env[1]
+		}
+	}
+
+	for key, value := range currEnvMap {
+		if _, ok := sysEnvMap[key]; !ok {
+			os.Setenv(key, value)
+		}
+	}
+}
